1a1296e2dde4ab8fa6e3d125600cda2940044cc3
Alex <awesomw.dynamics@gmail.com>
2018-01-16 21:14:14 +0300

    ww

--- tablica_bot.py
+++ tablica_bot.py
@@ -391,13 +391,13 @@ def free_text(message: telebot.types.Message):
 @server.route("/bot", methods=['POST'])
 def getMessage():
     #bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-    return "!", 200
+    return "200"
 
 @server.route("/")
 def webhook():
     bot.remove_webhook()
     bot.set_webhook(url="https://tablicabot.herokuapp.com/bot")
-    return "!", 200
+    return "200"
 
-server.run(host="0.0.0.0", port=5000)
+server.run(port=5000)
 server = Flask(__name__)
