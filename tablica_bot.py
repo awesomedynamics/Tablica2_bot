c4e5a6d68cc97da6d2b7c96af41b682ef5970a96
Alex <awesomw.dynamics@gmail.com>
2018-01-16 20:51:14 +0300

    fix port

--- tablica_bot.py
+++ tablica_bot.py
@@ -399,5 +399,5 @@ def webhook():
     bot.set_webhook(url="https://tablicabot.herokuapp.com/bot")
     return "!", 200
 
-server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
+server.run(host="0.0.0.0", port=8080)
 server = Flask(__name__)
