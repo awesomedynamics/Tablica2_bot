2dccc2f9da94471856dbe1a2fe262e171735f930
Alex <awesomw.dynamics@gmail.com>
2018-01-17 11:33:20 +0300

    mongodb functions moved to dedicated file

---
+++ mongodb_api.py
@@ -0,0 +1,84 @@
+#функция апдейтит базу контактом или продуктом
+def update_booking(chat_id, product = None, contact = None, people = None, date = None, length = None):
+
+    if product is not None:
+
+        bookings_coll.update_one(
+            {"chat_id": chat_id},
+            {
+                "$set": {"product": product}
+            }
+        )
+
+    if contact is not None:
+
+        bookings_coll.update_one(
+            {"chat_id": chat_id},
+            {
+                "$set": {"contact": contact}
+            }
+        )
+
+    if people is not None:
+
+        bookings_coll.update_one(
+            {"chat_id": chat_id},
+            {
+                "$set": {"people": people}
+            }
+        )
+
+    if date is not None:
+
+        bookings_coll.update_one(
+            {"chat_id": chat_id},
+            {
+                "$set": {"date": date}
+            }
+        )
+
+    if length is not None:
+
+        bookings_coll.update_one(
+            {"chat_id": chat_id},
+            {
+                "$set": {"length": length}
+            }
+        )
+
+#функция пишет лог в базу
+
+def update_log(chat_id = None, message = None):
+
+    if chat_id is not None and message is not None:
+        print(message)
+        username = str(message.chat.first_name) + " " + str(message.chat.last_name)
+
+        log_record = {
+                "chat_id" : message.chat.id,
+                "name" : username,
+                "time" : datetime.datetime.now(),
+                "message" : message.text
+            }
+        username = str(message.chat.first_name) + " " + str(message.chat.last_name)
+
+        log_coll.insert_one(log_record)
+
+def register_user(message):
+
+    existing_booking = bookings_coll.find_one({"chat_id":message.chat.id})
+    print(existing_booking)
+
+    if existing_booking is None:
+
+        username = str(message.chat.first_name) + " " + str(message.chat.last_name)
+
+        booking = {
+            "chat_id": message.chat.id,
+            "name": username,
+            "product": None,
+            "contact": None
+        }
+
+        # вставляем в монго запись - раз и навсегда
+        bookings_coll.insert_one(booking)
