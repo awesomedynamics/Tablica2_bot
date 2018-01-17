814e8713254a850774b208f95d72254dbc4e3035
Alex <awesomw.dynamics@gmail.com>
2018-01-18 01:03:31 +0300

    russification

--- tablica_bot.py
+++ tablica_bot.py
@@ -46,11 +46,11 @@ def start_command(message: telebot.types.Message):
 @bot.message_handler(func = lambda message: message.text is not None and message.text == "Офис")
 def  office(message: telebot.types.Message):
     reply_markup = types.ForceReply()
-    bot.send_message(chat_id=message.chat.id, text="Number of people in office:", reply_markup=reply_markup)
+    bot.send_message(chat_id=message.chat.id, text="количество сотрудников", reply_markup=reply_markup)
 
 
 # Обрабатываем ответ о кол-ве людей в офисе
-@bot.message_handler(func = lambda message: message.reply_to_message is not None and message.reply_to_message.text == "Number of people in office:")
+@bot.message_handler(func = lambda message: message.reply_to_message is not None and message.reply_to_message.text == "количество сотрудников:")
 def  office_options(message: telebot.types.Message):
 
     # убедимся что нам дали число сотрудников. Если там текст то заставим повторить и запустим функцию заново
@@ -60,7 +60,7 @@ def  office_options(message: telebot.types.Message):
         number_of_empl = message.text
         bot.send_message(chat_id=message.chat.id, text="что-то не так, введи, пожалуйста, ЧИСЛО сотрудников")
         reply_markup = types.ForceReply()
-        bot.send_message(chat_id=message.chat.id, text="Number of people in office:", reply_markup=reply_markup)
+        bot.send_message(chat_id=message.chat.id, text="количество сотрудников:", reply_markup=reply_markup)
         return None
 
     if number_of_empl < 4:
@@ -129,12 +129,12 @@ def book_coworking(message: telebot.types.Message):
 @bot.message_handler(func = lambda message: message.text is not None and message.text == "Мероприятие")
 def event(message: telebot.types.Message):
     reply_markup = types.ForceReply()
-    bot.send_message(chat_id=message.chat.id, text="Number of people in event:", reply_markup=reply_markup)
+    bot.send_message(chat_id=message.chat.id, text="количество гостей:", reply_markup=reply_markup)
     update_booking(chat_id=message.chat.id, product="event")
 
 
 # Обрабатываем ответ о кол-ве людей на Мероприятие
-@bot.message_handler(func = lambda message: message.reply_to_message is not None and message.reply_to_message.text == "Number of people in event:")
+@bot.message_handler(func = lambda message: message.reply_to_message is not None and message.reply_to_message.text == "количество гостей:")
 def event_size(message: telebot.types.Message):
 
     # убедимся что нам дали число сотрудников. Если там текст то заставим повторить и запустим функцию заново
@@ -144,7 +144,7 @@ def event_size(message: telebot.types.Message):
         number_of_ppl = message.text
         bot.send_message(chat_id=message.chat.id, text="что-то не так, введи, пожалуйста, ЧИСЛО людей на мероприятии")
         reply_markup = types.ForceReply()
-        bot.send_message(chat_id=message.chat.id, text="Number of people in office:", reply_markup=reply_markup)
+        bot.send_message(chat_id=message.chat.id, text="количество гостей:", reply_markup=reply_markup)
         return None
 
     get_calendar(message)
