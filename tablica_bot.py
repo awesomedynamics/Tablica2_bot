997e03a190c91e071a2f8a3da2fd5d4a85988ddb
Alex <awesomw.dynamics@gmail.com>
2018-01-17 16:45:42 +0300

    small fixes

--- tablica_bot.py
+++ tablica_bot.py
@@ -1,13 +1,12 @@
 import telebot
-import pymongo
 from telebot import types
 from pymongo import MongoClient
 import datetime
 from flask import Flask, request
 from flask_sslify import SSLify
 import os
-
-
+from mongodb_api import update_booking, update_log, register_user
+from telegramcalendar import create_calendar
 
 #подключаемся к монго
 client = MongoClient(os.environ["MONGODB_URL"], username = os.environ["MONGODB_USERNAME"], password = os.environ["MONGODB_PASSWORD"], authSource = os.environ["MONGODB_AUTHSOURCE"])
@@ -216,99 +215,7 @@ def main_menu(message: telebot.types.Message):
     bot.send_message(message.chat.id, "Так о чем же тебе рассказать ?",
                      reply_markup=markup)
 
-
-
-
-#функция апдейтит базу контактом или продуктом
-
-def update_booking(chat_id, product = None, contact = None, people = None, date = None, length = None):
-
-    if product is not None:
-
-        bookings_coll.update_one(
-            {"chat_id": chat_id},
-            {
-                "$set": {"product": product}
-            }
-        )
-
-    if contact is not None:
-
-        bookings_coll.update_one(
-            {"chat_id": chat_id},
-            {
-                "$set": {"contact": contact}
-            }
-        )
-
-    if people is not None:
-
-        bookings_coll.update_one(
-            {"chat_id": chat_id},
-            {
-                "$set": {"people": people}
-            }
-        )
-
-    if date is not None:
-
-        bookings_coll.update_one(
-            {"chat_id": chat_id},
-            {
-                "$set": {"date": date}
-            }
-        )
-
-    if length is not None:
-
-        bookings_coll.update_one(
-            {"chat_id": chat_id},
-            {
-                "$set": {"length": length}
-            }
-        )
-
-#функция пишет лог в базу
-
-def update_log(chat_id = None, message = None):
-
-    if chat_id is not None and message is not None:
-        print(message)
-        username = str(message.chat.first_name) + " " + str(message.chat.last_name)
-
-        log_record = {
-                "chat_id" : message.chat.id,
-                "name" : username,
-                "time" : datetime.datetime.now(),
-                "message" : message.text
-            }
-        username = str(message.chat.first_name) + " " + str(message.chat.last_name)
-
-        log_coll.insert_one(log_record)
-
-def register_user(message):
-
-    existing_booking = bookings_coll.find_one({"chat_id":message.chat.id})
-    print(existing_booking)
-
-    if existing_booking is None:
-
-        username = str(message.chat.first_name) + " " + str(message.chat.last_name)
-
-        booking = {
-            "chat_id": message.chat.id,
-            "name": username,
-            "product": None,
-            "contact": None
-        }
-
-        # вставляем в монго запись - раз и навсегда
-        bookings_coll.insert_one(booking)
-
-
-
 #календарь
-from telegramcalendar import create_calendar
 current_shown_dates={}
 
 def get_calendar(message):
